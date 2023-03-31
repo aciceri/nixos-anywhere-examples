045cac1651280d2689b5abc9a52d3b121394369d
Andrea Ciceri <andrea.ciceri@autistici.org>
2023-03-31 15:38:22 +0200

    Test

--- disk-config.nix
+++ disk-config.nix
@@ -1,73 +1,73 @@
-# Example to create a bios compatible gpt partition
 { lib, disks ? [ "/dev/nvme0n1" "/dev/nvme1n1" ], ... }: {
-  disk = lib.genAttrs disks (dev: {
-    device = dev;
-    type = "disk";
-    content = {
-      type = "table";
-      format = "gpt";
-      partitions = [
-        {
-          name = "boot";
-          type = "partition";
-          start = "0";
-          end = "1M";
-          part-type = "primary";
-          flags = ["bios_grub"];
-        }
-        {
-          type = "partition";
-          name = "ESP";
-          start = "1MiB";
-          end = "100MiB";
-          bootable = true;
-          content = {
-            type = "mdraid";
-            name = "boot";
-          };
-        }
-        {
-          name = "root";
-          type = "partition";
-          start = "100MiB";
-          end = "100%";
-          part-type = "primary";
-          bootable = true;
-          content = {
-            type = "lvm_pv";
-            vg = "pool";
-          };
-        }
-      ];
-    };
-  });
-  mdadm = {
-    boot = {
-      type = "mdadm";
-      level = 1;
-      metadata = "1.0";
+    disk = {
+    x = {
+      type = "disk";
+      device = builtins.elemAt disks 0;
       content = {
-        type = "filesystem";
-        format = "vfat";
-        mountpoint = "/boot";
+        type = "table";
+        format = "gpt";
+        partitions = [
+          {
+            type = "partition";
+            name = "ESP";
+            start = "0";
+            end = "64MiB";
+            fs-type = "fat32";
+            bootable = true;
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+            };
+          }
+          {
+            type = "partition";
+            name = "zfs";
+            start = "128MiB";
+            end = "100%";
+            content = {
+              type = "zfs";
+              pool = "zroot";
+            };
+          }
+        ];
+      };
+    };
+    y = {
+      type = "disk";
+      device = builtins.elemAt disks 1;
+      content = {
+        type = "table";
+        format = "gpt";
+        partitions = [
+          {
+            type = "partition";
+            name = "zfs";
+            start = "128MiB";
+            end = "100%";
+            content = {
+              type = "zfs";
+              pool = "zroot";
+            };
+          }
+        ];
       };
     };
   };
-  lvm_vg = {
-    pool = {
-      type = "lvm_vg";
-      lvs = {
+  zpool = {
+    zroot = {
+      type = "zpool";
+      mode = "mirror";
+      rootFsOptions = {
+        compression = "lz4";
+        "com.sun:auto-snapshot" = "false";
+      };
+
+      datasets = {
         root = {
-          type = "lvm_lv";
-          size = "100%FREE";
-          content = {
-            type = "filesystem";
-            format = "ext4";
-            mountpoint = "/";
-            mountOptions = [
-              "defaults"
-            ];
-          };
+          zfs_type = "filesystem";
+          options.mountpoint = "legacy";
+          mountpoint = "/";
         };
       };
     };
--- flake.nix
+++ flake.nix
@@ -16,6 +16,7 @@
           disko.devices = import ./disk-config.nix {
             lib = nixpkgs.lib;
           };
+          boot.initrd.availableKernelModules = [ "nvme" ];
           boot.loader.grub = {
             devices = [ "/dev/nvme0n1" "/dev/nvme1n1" ];
             efiSupport = true;
@@ -23,6 +24,8 @@
           };
           services.openssh.enable = true;
 
+          networking.hostId = "03259b66";
+          
           users.users.root.openssh.authorizedKeys.keys = [
             # change this to your ssh key
             "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDJmn7H6wxrxCHypvY74Z6pBr5G6v564NaUZb9xIILV92JEdpZzuTLLlP+JkMx/8MLRy+pC7prMwR+FhH+LaTm/9x3T6FYP/q9UIAL3cFwBAwj5XQXQKzx9f6pX/7iJrMfAUQ+ZrRUNJHt5Gl+8UypmDgnQLuv5vmQSMRzKnUPuu4lCJtWOpSPhXffz3Ec1tm5nAMuxIMRPY91PYu1fMLlFrjB1FX1goVHKB1uWx16GjJszYCVbN6xcPac0sgUg+qNGBhWkUh0F073rhepQJeWp5FtwIxe2zRsZBxxTy5qxNLmHzBeNDxlOkcy2/Lr+BxVy+mhF/2fJziX80/bWSEA1 (none)"
